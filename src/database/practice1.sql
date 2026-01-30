144fe72aafee62b2c8292942acece193c8d30324
peri2532 <peri253278@gmail.com>
2026-01-30 11:23:16 +0900

    끄읕~

---
+++ src/database/practice1.sql
@@ -0,0 +1,17 @@
+-- [문제 1]
+create database my_db;
+
+-- [문제 2]
+show databases;
+
+-- [문제 3]
+use my_db;
+
+-- [문제 4]
+drop database my_db;
+
+-- [문제 5]
+drop database if exists company_db;
+create database company_db;
+use company_db;
+show databases;
